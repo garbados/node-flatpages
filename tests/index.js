dbfc128969931adc1b398ee10fe4a0fc1fd64593
Diana Thayer <garbados@gmail.com>
2017-11-15 15:48:37 -0800

    [0.2.1] bug fix release

--- tests/index.js
+++ tests/index.js
@@ -7,7 +7,11 @@ tap.test('flatpages', (group) => {
     fsMock({
       'entries': {
         'hello.md': '# wow',
-        'advanced.md': 'title: Hello, world!\n\n\n# omg wat\n'
+        'advanced.md': 'title: Hello, world!\n\n# omg wat\n',
+        'wrong.png': 'no thank you',
+        'drafts': {
+          'shitpost.md': 'cw: i can\'t believe it\'s $CURRENT_YEAR and\n\ni haven\'t got my $PUNCHLINE'
+        }
       }
     })
     done()
@@ -22,7 +26,10 @@ tap.test('flatpages', (group) => {
     flatpages('entries')
       .then((pages) => {
         test.equal(pages['hello.md'].html, '<h1 id="wow">wow</h1>', 'Should process markdown correctly.')
+        test.equal(pages['hello.md'].meta, undefined)
         test.equal(pages['advanced.md'].meta.title, 'Hello, world!', 'Should process YAML correctly.')
+        test.equal(pages['wrong.png'], undefined)
+        test.ok(pages['drafts']['shitpost.md'].meta.cw)
         test.end()
       })
   })
